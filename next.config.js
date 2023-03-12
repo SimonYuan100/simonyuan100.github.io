af98a861c63834168179187b7777c7b12341a9e1
simon <simonyuan100@163.com>
2023-03-12 23:41:54 +0800

    feat: config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
